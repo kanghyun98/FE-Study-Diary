35cf5cb63e2d904adcbf59c6babd72ae4bfe7279
20170781 <lkh981107@naver.com>
2021-06-29 23:55:59 +0900

    fix Modal

--- src/components/box.js
+++ src/components/box.js
@@ -3,80 +3,71 @@ import { makeModal } from "./modal.js";
 
 const allData = [studyData06, studyData07];
 
-export function makeBox(data) {
-  const section = document.querySelector(".result-section");
+const section = document.querySelector(".result-section");
 
-  //container
-  const divContainer = document.createElement("div");
-  divContainer.className = "result-section__container";
+export function makeBox() {
+  for (let k = 0; k < allData.length; k++) {
+    //container
+    const divContainer = document.createElement("div");
+    divContainer.className = "result-section__container";
 
-  //container-month
-  const h3Month = document.createElement("h3");
-  h3Month.className = "result-section__month";
-  h3Month.innerText = `${data[0].date.slice(0, 4)}년 ${data[0].date.slice(
-    5,
-    7
-  )}월`;
+    //container-boxes
+    const divBoxes = document.createElement("div");
+    divBoxes.className = "result-section__boxes";
 
-  //container-boxes
-  const divBoxes = document.createElement("div");
-  divBoxes.className = "result-section__boxes";
+    //container-month
+    const h3Month = document.createElement("h3");
+    h3Month.className = "result-section__month";
+    h3Month.innerText = `${allData[k][0].date.slice(0, 4)}년 ${allData[
+      k
+    ][0].date.slice(5, 7)}월`;
 
-  //boxes - box
-  for (let i = 0; i < data.length; i++) {
-    let divBox = document.createElement("div");
-    divBox.className = "result-section__box";
+    //boxes - box
+    for (let i = 0; i < allData[k].length; i++) {
+      let divBox = document.createElement("div");
+      divBox.className = "result-section__box";
 
-    //box - p
-    let pDate = document.createElement("p");
-    pDate.className = "result-section__date";
-    pDate.innerText = `${data[i].date.slice(5, 7)}월 ${data[i].date.slice(
-      8
-    )}일`;
+      //box - p
+      let pDate = document.createElement("p");
+      pDate.className = "result-section__date";
+      pDate.innerText = `${allData[k][i].date.slice(5, 7)}월 ${allData[k][
+        i
+      ].date.slice(8)}일`;
 
-    //box - ul
-    let ulList = document.createElement("ul");
-    ulList.className = "result-section__list";
+      //box - ul
+      let ulList = document.createElement("ul");
+      ulList.className = "result-section__list";
 
-    //ul - li - div
-    let divLists = [];
-    let liLists = [];
-    for (let j = 0; j < data[i].studyList.length; j++) {
-      liLists[j] = document.createElement("li");
+      //ul - li - div
+      let divLists = [];
+      let liLists = [];
+      for (let j = 0; j < allData[k][i].studyList.length; j++) {
+        liLists[j] = document.createElement("li");
 
-      divLists[j] = document.createElement("div");
-      divLists[j].className = "result-section__lists";
-      divLists[j].innerText = data[i].studyList[j].subject;
-      liLists[j].appendChild(divLists[j]);
-      ulList.appendChild(liLists[j]);
+        divLists[j] = document.createElement("div");
+        divLists[j].className = "result-section__lists";
+        divLists[j].innerText = allData[k][i].studyList[j].subject;
+        liLists[j].appendChild(divLists[j]);
+        ulList.appendChild(liLists[j]);
 
-      //modal 삽입
-      const madeModal = makeModal(data[i].studyList[j]);
-      // madeModal.id = `modalId${i}${j}`; //modal 붙여주기 위해           //
+        //modal 삽입
+        const madeModal = makeModal(allData[k][i].studyList[j]);
+        madeModal.id = `modalId${k}${i}${j}`; //modal 붙여주기 위해
 
-      // const hiddenModal = document.querySelector(`#modalId${i}${j}`);  //
+        divLists[j].addEventListener("click", () => {
+          madeModal.classList.toggle("hidden");
+        });
+        liLists[j].appendChild(madeModal);
+      }
 
-      divLists[j].addEventListener("click", () => {
-        //
-        hiddenModal.classList.toggle("hidden");
-      });
-      liLists[j].appendChild(madeModal);
+      divBox.appendChild(pDate);
+      divBox.appendChild(ulList);
+      divBoxes.appendChild(divBox);
     }
-
-    divBox.appendChild(pDate);
-    divBox.appendChild(ulList);
-    divBoxes.appendChild(divBox);
+    divContainer.appendChild(h3Month);
+    divContainer.appendChild(divBoxes);
+    section.appendChild(divContainer);
   }
-
-  divContainer.appendChild(h3Month);
-  divContainer.appendChild(divBoxes);
-  section.appendChild(divContainer);
 }
 
-makeBox(studyData06);
-makeBox(studyData07);
-
-const ttt = document.querySelector(".result-section");
-ttt.addEventListener("click", () => {
-  console.log("hey");
-});
+makeBox();
--- src/components/modal.js
+++ src/components/modal.js
@@ -8,11 +8,11 @@ export function makeModal(modalData) {
   const modalContents = document.createElement("div");
   modalContents.className = "modal-wrapper__contents";
 
-  const spanSubject = document.createElement("span");
-  spanSubject.innerText = `제목: ${modalData.subject}`;
+  const h2Subject = document.createElement("h2");
+  h2Subject.innerText = `${modalData.subject}`;
 
   const spanArr = document.createElement("span");
-  spanArr.innerText = `내용: ${modalData.arr}`;
+  spanArr.innerText = `${modalData.arr}`;
 
   const aMyLink = document.createElement("a");
   aMyLink.innerText = `정리 노트: ${modalData.myLink}`;
@@ -31,12 +31,9 @@ export function makeModal(modalData) {
   const modalOverlay = document.createElement("div");
   modalOverlay.className = "modal-wrapper__overlay";
 
-  const modalAll = document.querySelector(".modal-wrapper");
-  modalOverlay.addEventListener("click", () =>
-    modalAll.classList.add("hidden")
-  );
+  modalOverlay.addEventListener("click", () => modal.classList.add("hidden"));
 
-  modalContents.appendChild(spanSubject);
+  modalContents.appendChild(h2Subject);
   modalContents.appendChild(spanArr);
   modalContents.appendChild(aMyLink);
   modalContents.appendChild(ulRefLink);
@@ -46,16 +43,3 @@ export function makeModal(modalData) {
 
   return modal;
 }
-
-export function showModal() {}
-
-// for (let i = 0; i < allData.length; i++) {
-//   for (let j = 0; j < allData[i].length; j++) {
-//     for (let k = 0; k < allData[i][j].studyList.length; k++) {
-//       let liAll = document.querySelector(`.type${j}${k}`);
-//       let myStudyList = allData[i][j].studyList[k];
-//       liAll.addEventListener("click", makeModal(myStudyList));
-//       ulList.appendChild(liAll);
-//     }
-//   }
-// }
