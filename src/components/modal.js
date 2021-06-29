fd382726e72423b461cd23020d7c6809abe4305b
20170781 <lkh981107@naver.com>
2021-06-30 01:37:21 +0900

    fix Modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -8,21 +8,44 @@ export function makeModal(modalData) {
   const modalContents = document.createElement("div");
   modalContents.className = "modal-wrapper__contents";
 
-  const h2Subject = document.createElement("h2");
-  h2Subject.innerText = `${modalData.subject}`;
+  const h2Subject = document.createElement("h2"); //제목
+  h2Subject.innerText = modalData.subject;
 
-  const spanArr = document.createElement("span");
-  spanArr.innerText = `${modalData.arr}`;
+  const spanArr = document.createElement("span"); //요약
+  spanArr.className = "arr";
+  spanArr.innerText = modalData.arr;
+
+  const divMy = document.createElement("div"); //정리 노트
+  divMy.className = "myLink";
+
+  const spanMyMent = document.createElement("span");
+  spanMyMent.innerText = "정리 노트";
 
   const aMyLink = document.createElement("a");
-  aMyLink.innerText = `정리 노트: ${modalData.myLink}`;
-  aMyLink.setAttribute("href", modalData.myLink);
+  if (modalData.myLink.length > 0) {
+    aMyLink.setAttribute("href", modalData.myLink);
+    aMyLink.innerText = "My Link is here!";
+  } else {
+    aMyLink.innerText = "아직 정리하지 않았네요;)";
+  }
+
+  const divRef = document.createElement("div"); //참조 링크
+  divRef.className = "refLink";
+
+  const spanRefMent = document.createElement("span");
+  spanRefMent.innerText = "참조 링크";
 
   const ulRefLink = document.createElement("ul");
   modalData.refLink.forEach((element) => {
     const liLinks = document.createElement("li");
     const aLinks = document.createElement("a");
+    if (element.length < 50) {
+      aLinks.innerText = element;
+    } else {
+      aLinks.innerText = "Ref Link is here!";
+    }
     aLinks.setAttribute("href", element);
+
     liLinks.appendChild(aLinks);
     ulRefLink.appendChild(liLinks);
   });
@@ -35,8 +58,14 @@ export function makeModal(modalData) {
 
   modalContents.appendChild(h2Subject);
   modalContents.appendChild(spanArr);
-  modalContents.appendChild(aMyLink);
-  modalContents.appendChild(ulRefLink);
+
+  divMy.appendChild(spanMyMent);
+  divMy.appendChild(aMyLink);
+  modalContents.appendChild(divMy);
+
+  divRef.appendChild(spanRefMent);
+  divRef.appendChild(ulRefLink);
+  modalContents.appendChild(divRef);
 
   modal.appendChild(modalContents);
   modal.appendChild(modalOverlay);
