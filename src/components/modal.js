ca3a5d0eb8e5880f0f5a14ab9c1d8d7930a13ee3
20170781 <lkh981107@naver.com>
2021-07-01 15:35:55 +0900

    Refactoring: fix Modal for OOP

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,74 +1,104 @@
-export function makeModal(modalData) {
-  // modal 전체
-  const modal = document.createElement("div");
-  modal.className = "modal-wrapper";
-  modal.classList.add("hidden");
+export default class Modal {
+  constructor({ $target }) {
+    this.isVisible = false;
+    this.data = null;
+    this.modalWrapper = document.createElement("div");
+    this.modalWrapper.className = "modal-wrapper";
+    this.modalWrapper.classList.add("hidden");
 
-  // modal 내부 (컨텐츠 보여주기) div - span - a, ul(-li-a)
-  const modalContents = document.createElement("div");
-  modalContents.className = "modal-wrapper__contents";
+    $target.appendChild(this.modalWrapper);
 
-  const h2Subject = document.createElement("h2"); //제목
-  h2Subject.innerText = modalData.subject;
-
-  const spanArr = document.createElement("span"); //요약
-  spanArr.className = "arr";
-  spanArr.innerText = modalData.arr;
-
-  const divMy = document.createElement("div"); //정리 노트
-  divMy.className = "myLink";
-
-  const spanMyMent = document.createElement("span");
-  spanMyMent.innerText = "정리 노트";
-
-  const aMyLink = document.createElement("a");
-  if (modalData.myLink.length > 0) {
-    aMyLink.setAttribute("href", modalData.myLink);
-    aMyLink.innerText = "My Link is here!";
-  } else {
-    aMyLink.innerText = "아직 정리하지 않았네요;)";
+    this.render();
   }
 
-  const divRef = document.createElement("div"); //참조 링크
-  divRef.className = "refLink";
+  toggleModal() {
+    this.isVisible = !this.isVisible; //필요한가?
 
-  const spanRefMent = document.createElement("span");
-  spanRefMent.innerText = "참조 링크";
+    const modal = document.querySelector(".modal-wrapper");
+    modal.classList.toggle("hidden"); //필요한가?
+  }
 
-  const ulRefLink = document.createElement("ul");
-  modalData.refLink.forEach((element) => {
-    const liLinks = document.createElement("li");
-    const aLinks = document.createElement("a");
-    if (element.length < 50) {
-      aLinks.innerText = element;
+  setState(data) {
+    this.data = data;
+    this.toggleModal();
+    this.render();
+  }
+
+  onClose() {
+    this.toggleModal();
+    this.data = null;
+    this.modalWrapper.innerHTML = "";
+  }
+
+  render() {
+    if (!this.isVisible) return; //필요한가?
+
+    // modal 내부 (컨텐츠 보여주기) div - span - a, ul(-li-a)
+    const modalContents = document.createElement("div");
+    modalContents.className = "modal-wrapper__contents";
+
+    const h2Subject = document.createElement("h2"); //제목
+    h2Subject.innerText = this.data.subject;
+
+    const spanArr = document.createElement("span"); //요약
+    spanArr.className = "arr";
+    spanArr.innerText = this.data.arr;
+
+    const divMy = document.createElement("div"); //정리 노트
+    divMy.className = "myLink";
+
+    const spanMyMent = document.createElement("span");
+    spanMyMent.innerText = "정리 노트";
+
+    const aMyLink = document.createElement("a");
+    if (this.data.myLink.length > 0) {
+      aMyLink.setAttribute("href", this.data.myLink);
+      aMyLink.innerText = "My Link is here!";
     } else {
-      aLinks.innerText = "Ref Link is here!";
+      aMyLink.innerText = "아직 정리하지 않았네요;)";
     }
-    aLinks.setAttribute("href", element);
 
-    liLinks.appendChild(aLinks);
-    ulRefLink.appendChild(liLinks);
-  });
+    const divRef = document.createElement("div"); //참조 링크
+    divRef.className = "refLink";
 
-  // modal 외부 (클릭 시 hidden)
-  const modalOverlay = document.createElement("div");
-  modalOverlay.className = "modal-wrapper__overlay";
+    const spanRefMent = document.createElement("span");
+    spanRefMent.innerText = "참조 링크";
 
-  modalOverlay.addEventListener("click", () => modal.classList.add("hidden"));
+    const ulRefLink = document.createElement("ul");
+    this.data.refLink.forEach((element) => {
+      const liLinks = document.createElement("li");
+      const aLinks = document.createElement("a");
+      if (element.length < 50) {
+        aLinks.innerText = element;
+      } else {
+        aLinks.innerText = "Ref Link is here!";
+      }
+      aLinks.setAttribute("href", element);
 
-  modalContents.appendChild(h2Subject);
-  modalContents.appendChild(spanArr);
+      liLinks.appendChild(aLinks);
+      ulRefLink.appendChild(liLinks);
+    });
 
-  divMy.appendChild(spanMyMent);
-  divMy.appendChild(aMyLink);
-  modalContents.appendChild(divMy);
+    // modal 외부 (클릭 시 hidden)
+    const modalOverlay = document.createElement("div");
+    modalOverlay.className = "modal-wrapper__overlay";
 
-  divRef.appendChild(spanRefMent);
-  divRef.appendChild(ulRefLink);
-  modalContents.appendChild(divRef);
+    modalOverlay.addEventListener("click", () => {
+      this.onClose();
+    });
 
-  modal.appendChild(modalContents);
-  modal.appendChild(modalOverlay);
+    modalContents.appendChild(h2Subject);
+    modalContents.appendChild(spanArr);
 
-  return modal;
+    divMy.appendChild(spanMyMent);
+    divMy.appendChild(aMyLink);
+    modalContents.appendChild(divMy);
+
+    divRef.appendChild(spanRefMent);
+    divRef.appendChild(ulRefLink);
+    modalContents.appendChild(divRef);
+
+    this.modalWrapper.appendChild(modalContents);
+    this.modalWrapper.appendChild(modalOverlay);
+  }
 }
