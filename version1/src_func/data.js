b72cf46497a692531dcd8a77558d73cb2823ed05
20170781 <lkh981107@naver.com>
2021-07-03 18:55:05 +0900

    move previous version's files

---
+++ version1/src_func/data.js
@@ -0,0 +1,306 @@
+export const studyData06 = [
+  {
+    date: "2021.06.26",
+    studyList: [
+      {
+        subject: "시맨틱 웹",
+        arr: "시맨틱 태그를 사용한 HTML, 웹페이지의 컨텐츠가 명확한 의미 가짐, HTML 태그 수정 필요",
+        myLink: "",
+        refLink: [
+          "https://www.w3schools.com/html/html5_semantic_elements.asp",
+          "https://velog.io/@hyeon930/%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%EC%97%90-%EB%94%B0%EB%A5%B8-%ED%8D%BC%EB%B8%94%EB%A6%AC%EC%8B%B1-%EC%8B%9C%EB%A7%A8%ED%8B%B1-%EC%9B%B9-%EB%B0%98%EC%9D%91%ED%98%95-%ED%8E%98%EC%9D%B4%EC%A7%80",
+          "https://soeunlee.medium.com/%EC%8B%9C%EB%A7%A8%ED%8B%B1%ED%95%98%EA%B2%8C-html%EC%9D%84-%EC%A7%A0%EB%8B%A4%EB%8A%94-%EA%B2%83-90612ffc988e",
+        ],
+      },
+      {
+        subject: "반응형 페이지 (적용)",
+        arr: "화면 크기에 따른 페이지의 반응(flexbox)",
+        myLink: "",
+        refLink: [
+          "https://velog.io/@hyeon930/%EC%9A%94%EA%B5%AC%EC%82%AC%ED%95%AD%EC%97%90-%EB%94%B0%EB%A5%B8-%ED%8D%BC%EB%B8%94%EB%A6%AC%EC%8B%B1-%EC%8B%9C%EB%A7%A8%ED%8B%B1-%EC%9B%B9-%EB%B0%98%EC%9D%91%ED%98%95-%ED%8E%98%EC%9D%B4%EC%A7%80",
+        ],
+      },
+      {
+        subject: "JSON 파일 구조 (적용)",
+        arr: '"데이터이름":값, 객체{}, 배열[], 추후에 class나 function 활용해 만들기  ',
+        myLink: "",
+        refLink: [
+          "https://developer.mozilla.org/ko/docs/Learn/JavaScript/Objects/JSON",
+          "http://tcpschool.com/json/json_basic_structure",
+        ],
+      },
+      {
+        subject: "defer/async",
+        arr: "브라우저 동작 방식 제어",
+        myLink: "",
+        refLink: [
+          "https://ko.javascript.info/script-async-defer",
+          "https://www.youtube.com/watch?v=tJieVCgGzhs&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=2",
+        ],
+      },
+      {
+        subject: "라이브러리, 프레임워크, API, ABI",
+        arr: "맛보기, 추후에 정리 필요",
+        myLink: "",
+        refLink: [
+          "https://eine.tistory.com/entry/라이브러리-API-ABI-뜻-비교-정리",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.06.27",
+    studyList: [
+      {
+        subject: "let / const",
+        arr: "Variable(Mutable): let  / Constant(Immutable): const",
+        myLink:
+          "https://www.notion.so/Variable-let-Constant-const-b5cf20cdf27f4b44b0d8c564b4186146",
+        refLink: [
+          "https://www.youtube.com/watch?v=_DLhUBWsRtw&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=6",
+        ],
+      },
+      {
+        subject: "Class",
+        arr: "prototype을 활용하여 다른 언어들의 class와 유사한 기능 생성",
+        myLink: "https://www.notion.so/Class-bb4a0bf7d80340f79fcd91cff28f10f2",
+        refLink: [
+          "https://www.youtube.com/watch?v=_DLhUBWsRtw&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=8",
+        ],
+      },
+      {
+        subject: "비동기 처리",
+        arr: "script 불러올 때 코드의 실행을 멈추지 않는 것",
+        myLink: "https://www.notion.so/a0f0183c3d434182b103046cb2baaef3",
+        refLink: [
+          "https://joshua1988.github.io/web-development/javascript/javascript-asynchronous-operation/",
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "콜백 함수",
+        arr: "다른 코드의 인자로 넘겨주는 함수",
+        myLink: "https://www.notion.so/13c20635b4be4f0a9193c7218c5c6040",
+        refLink: [
+          "https://joshua1988.github.io/web-development/javascript/javascript-asynchronous-operation/",
+          "https://www.youtube.com/watch?v=s1vpVCrT8f4&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=11",
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "Promise",
+        arr: "콜백 지옥 해결법",
+        myLink: "",
+        refLink: [
+          "https://joshua1988.github.io/web-development/javascript/promise-for-beginners/",
+          "https://www.youtube.com/watch?v=JB_yU6Oe2eE&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=12",
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "async & await",
+        arr: "콜백 함수와 프로미스의 단점 보완, 개발자가 읽기 좋은 코드 작성 가능하게 해줌",
+        myLink: "",
+        refLink: [
+          "https://joshua1988.github.io/web-development/javascript/js-async-await/",
+          "https://www.youtube.com/watch?v=aoQSOZfz3vQ&list=PLv2d7VI9OotTVOL4QmPfvJWPJvkmv6h-2&index=13",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.06.28",
+    studyList: [
+      {
+        subject: "Web Storage API (적용)",
+        arr: "임시 저장 장소",
+        myLink:
+          "https://www.notion.so/Web-Storage-API-2cc64fee30544cb98a1f2dab2057523e",
+        refLink: [
+          "https://velog.io/@hyeon930/%EC%83%88%EB%A1%9C%EA%B3%A0%EC%B9%A8-%ED%9B%84%EC%97%90%EB%8F%84-%EA%B2%B0%EA%B3%BC-%ED%99%94%EB%A9%B4-%EC%9C%A0%EC%A7%80%ED%95%98%EA%B8%B0-Web-Storage-API",
+          "https://developer.mozilla.org/ko/docs/Web/API/Web_Storage_API",
+        ],
+      },
+      {
+        subject: "Search (적용)",
+        arr: "검색 로직 구현, 새로고침 후에도 화면 유지",
+        myLink: "",
+        refLink: [
+          "https://velog.io/@hyeon930/%EC%9C%A0%EC%A0%80%EC%9D%98-%EC%9E%85%EB%A0%A5%EC%97%90-%EB%B0%98%EC%9D%91%ED%95%98%EA%B8%B0-%EB%A1%9C%EB%94%A9-%ED%99%94%EB%A9%B4%EA%B3%BC-%EA%B2%B0%EA%B3%BC-%EC%97%86%EC%9D%8C-Event-loop-Class",
+          "https://velog.io/@hyeon930/%EC%83%88%EB%A1%9C%EA%B3%A0%EC%B9%A8-%ED%9B%84%EC%97%90%EB%8F%84-%EA%B2%B0%EA%B3%BC-%ED%99%94%EB%A9%B4-%EC%9C%A0%EC%A7%80%ED%95%98%EA%B8%B0-Web-Storage-API",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.06.29",
+    studyList: [
+      {
+        subject: "Search Loading page (적용)",
+        arr: "검색 로딩 화면 구현",
+        myLink: "",
+        refLink: [
+          "https://velog.io/@hyeon930/%EC%9C%A0%EC%A0%80%EC%9D%98-%EC%9E%85%EB%A0%A5%EC%97%90-%EB%B0%98%EC%9D%91%ED%95%98%EA%B8%B0-%EB%A1%9C%EB%94%A9-%ED%99%94%EB%A9%B4%EA%B3%BC-%EA%B2%B0%EA%B3%BC-%EC%97%86%EC%9D%8C-Event-loop-Class",
+        ],
+      },
+      {
+        subject: "bubbling / capturing ",
+        arr: "하단 요소의 이벤트 발생 시, 최상단 부모 요소까지의 핸들러 동작 ",
+        myLink: "",
+        refLink: ["https://ko.javascript.info/bubbling-and-capturing"],
+      },
+      {
+        subject: "Modal (적용)",
+        arr: "클릭 시 모달 화면 구축, 외부화면 클릭 시 종료",
+        myLink: "",
+        refLink: [
+          "https://1ilsang.dev/2020-01-07/devlog/make-modal",
+          "https://velog.io/@hyeon930/Modal-%EB%A7%8C%EB%93%A4%EA%B8%B0-Event-propagation",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.06.30",
+    studyList: [
+      {
+        subject: "실행 컨텍스트",
+        arr: "실행할 코드에 제공할 환경 정보들을 모아놓은 객체",
+        myLink: "https://www.notion.so/a886127eea944cb6ab2e32bfd474c48f",
+        refLink: [
+          "https://im-designloper.tistory.com/21",
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "함수 선언문과 함수 표현식",
+        arr: "함수 정의 방법",
+        myLink: "https://www.notion.so/ef577d785421444db530090baf039c4a",
+        refLink: [
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "this",
+        arr: "어떤 방식으로 함수를 호출하느냐에 따라 값이 달라짐",
+        myLink: "https://www.notion.so/this-96374298462846ee9ec20950294b85a7",
+        refLink: [
+          "https://https://im-designloper.tistory.com/23?category=1147115.tistory.com/66",
+          "http://book.interpark.com/product/BookDisplay.do?_method=detail&sc.prdNo=316439749&gclid=CjwKCAjwrPCGBhALEiwAUl9X03UWvMKCa3wT3RJ_3kQKn2ocDnJPbHL2Oysl5litbzwHa-Ny0WeAnxoCX-8QAvD_BwE",
+        ],
+      },
+      {
+        subject: "oop 구현",
+        arr: "객체지향 프로그래밍으로 리팩토링",
+        myLink: "",
+        refLink: [
+          "https://parksb.github.io/article/1.html",
+          "https://github.com/woohyeonjo/ilovecat-javascript",
+        ],
+      },
+    ],
+  },
+];
+
+export const studyData07 = [
+  {
+    date: "2021.07.01",
+    studyList: [
+      {
+        subject: "WebAssembly",
+        arr: "Model, View, Controller",
+        myLink: "https://newindow.tistory.com/66",
+        refLink: [
+          "https://developers.kakao.com/",
+          "https://www.naver.com/",
+          "https://www.google.co.kr/",
+        ],
+      },
+      {
+        subject: "Chrome의 새로운 변화",
+        arr: "CSS class-name 방법론",
+        myLink: "https://nykim.work/15",
+        refLink: [
+          "https://newindow.tistory.com/66",
+          "https://github.com/woohyeonjo/ilovecat-javascript",
+          "https://developer.mozilla.org/ko/docs/Learn/JavaScript/Objects/JSON",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.07.02",
+    studyList: [
+      {
+        subject: "WebAssembly",
+        arr: "Model, View, Controller",
+        myLink: "https://newindow.tistory.com/66",
+        refLink: [
+          "https://developers.kakao.com/",
+          "https://www.naver.com/",
+          "https://www.google.co.kr/",
+        ],
+      },
+      {
+        subject: "Chrome의 새로운 변화",
+        arr: "CSS class-name 방법론",
+        myLink: "https://nykim.work/15",
+        refLink: [
+          "https://newindow.tistory.com/66",
+          "https://github.com/woohyeonjo/ilovecat-javascript",
+          "https://developer.mozilla.org/ko/docs/Learn/JavaScript/Objects/JSON",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.07.03",
+    studyList: [
+      {
+        subject: "WebAssembly",
+        arr: "Model, View, Controller",
+        myLink: "https://newindow.tistory.com/66",
+        refLink: [
+          "https://developers.kakao.com/",
+          "https://www.naver.com/",
+          "https://www.google.co.kr/",
+        ],
+      },
+      {
+        subject: "Chrome의 새로운 변화",
+        arr: "CSS class-name 방법론",
+        myLink: "https://nykim.work/15",
+        refLink: [
+          "https://newindow.tistory.com/66",
+          "https://github.com/woohyeonjo/ilovecat-javascript",
+          "https://developer.mozilla.org/ko/docs/Learn/JavaScript/Objects/JSON",
+        ],
+      },
+    ],
+  },
+  {
+    date: "2021.07.04",
+    studyList: [
+      {
+        subject: "재밌게해보장",
+        arr: "Model, View, Controller",
+        myLink: "https://newindow.tistory.com/66",
+        refLink: [
+          "https://developers.kakao.com/",
+          "https://www.naver.com/",
+          "https://www.google.co.kr/",
+        ],
+      },
+      {
+        subject: "아자아자",
+        arr: "CSS class-name 방법론",
+        myLink: "https://nykim.work/15",
+        refLink: [
+          "https://newindow.tistory.com/66",
+          "https://github.com/woohyeonjo/ilovecat-javascript",
+          "https://developer.mozilla.org/ko/docs/Learn/JavaScript/Objects/JSON",
+        ],
+      },
+    ],
+  },
+];
